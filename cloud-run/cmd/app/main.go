1efaa2f257fdbdcfd95b8e6608758b40525494a0
Jonas Borges Leao de Moraes <jonas.moraes@eldorado.org.br>
2024-05-02 08:53:35 -0300

    feat: :sparkles: modulo webserver
    Criando o modulo web-server.

--- cloud-run/cmd/app/main.go
+++ cloud-run/cmd/app/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/JonasBorgesLM/go/clound-run/internal/infra/webserver"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+	port := "8080"
+
+	webserver := webserver.NewWebServer(port)
+	webserver.AddHandler("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Printf("Server listening on %v\n", port)
+	webserver.Start()
 }
--- cloud-run/go.mod
+++ cloud-run/go.mod
@@ -1,3 +1,5 @@
 module github.com/JonasBorgesLM/go/clound-run
 
 go 1.21.3
+
+require github.com/go-chi/chi/v5 v5.0.12
